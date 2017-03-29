de5ccb4baa2d80bc050aa475c8f95a1b51f3bd56
XXZ <161250170@mail.smail.nju.edu.cn>
2017-03-30 00:30:46 +0800

    Match方法0.0可以用啦

--- Test/Main.java
+++ Test/Main.java
@@ -1,24 +1,32 @@
 import data.BoardInfor;
-import logic.Judge;
 import logic.Match;
 
 import java.util.Arrays;
+
+import static data.CD.BOARD_SIZE;
 
 /**
  * Created by xuxiangzhe on 2017/3/23.
  */
 public class Main {
     public static void main(String[] args) {
-        char[][] sample={{'a','b','b','b','c','c','a','d'},
-                        {'a','b','b','b','c','c','a','d'},
-                {'d','b','b','b','d','c','d','a'},
-                {'a','b','b','b','c','c','a','d'},
-                {'a','b','b','b','c','c','a','d'},
-                {'d','b','b','b','d','c','d','a'},
-                {'a','b','b','b','c','c','a','d'},
-                {'a','b','b','b','c','c','a','d'}};
+        char[][] sample = {{'a', 'b', 'B', 'b', 'c', 'c', 'a', 'd'},
+                {'a', 'b', 'b', 'b', 'c', 'c', 'a', 'd'},
+                {'d', 'b', 'b', 'b', 'b', 'c', 'd', 'a'},
+                {'a', 'c', 'c', 'a', 'c', 'c', 'c', 'd'},
+                {'a', 'c', 'c', 'a', 'b', 'b', 'a', 'd'},
+                {'d', 'd', 'd', 'd', 'd', 'c', 'd', 'a'},
+                {'a', 'c', 'c', 'd', 'c', 'c', 'a', 'd'},
+                {'a', 'd', 'd', 'd', 'c', 'c', 'a', 'd'}};
         BoardInfor.setInformation(sample);
-        System.out.println(Judge.judgeMatch3('b'));
-        System.out.println(Arrays.deepToString(BoardInfor.getInformation()));
+        Match.match();
+        print(BoardInfor.getInformation());
+    }
+
+    public static void print(char[][] chars) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            System.out.println(Arrays.toString(chars[i]));
+        }
+
     }
 }
--- src/logic/Match.java
+++ src/logic/Match.java
@@ -1,8 +1,51 @@
 package logic;
 
+import data.BoardInfor;
+
+import static data.CD.BOARD_SIZE;
+
 /**
  * 呀，对，就是这里，还没写……
  */
 public class Match {
-//TODO:.......
+    private static char[][] src;
+
+    public static void match() {
+        src = BoardInfor.getInformation();
+        matchRow();
+        matchCol();
+        BoardInfor.setInformation(src);
+    }
+
+    private static void matchRow() {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            src[i] = replace(src[i]);
+        }
+    }
+
+    //这 个 方 法 ……  也 太 蠢 了  ┻━┻︵╰(‵□′)╯︵┻━┻
+    private static void matchCol() {
+        char[] temp = new char[BOARD_SIZE];
+        for (int j = 0; j < BOARD_SIZE; j++) {
+
+            for (int i = 0; i < BOARD_SIZE; i++) {
+                temp[i] = src[i][j];
+            }
+
+            temp = replace(temp);
+
+            for (int i = 0; i < BOARD_SIZE; i++) {
+                src[i][j] = temp[i];
+            }
+        }
+    }
+
+    //这 个 方 法 写 的 实 在 是 太 难 看 了 ……    ⁄(⁄ ⁄ ⁄ω⁄ ⁄ ⁄)⁄
+    private static char[] replace(char[] chars) {
+        return new String(chars).replaceAll("(?i)aaaaa", "AAAAA").replaceAll("(?i)bbbbb", "BBBBB").replaceAll("(?i)ccccc", "CCCCC").
+                replaceAll("(?i)ddddd", "DDDDD").replaceAll("(?i)aaaa", "AAAA").replaceAll("(?i)bbbb", "BBBB").
+                replaceAll("(?i)cccc", "CCCC").replaceAll("(?i)dddd", "DDDD").replaceAll("(?i)aaa", "AAA").
+                replaceAll("(?i)bbb", "BBB").replaceAll("(?i)ccc", "CCC").replaceAll("(?i)ddd", "DDD").toCharArray();
+    }
+
 }
