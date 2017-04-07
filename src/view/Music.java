dd7732dc6bc834736ce5c0943f9767b174286a9f
wym0120 <598251106@qq.com>
2017-04-07 09:52:40 +0800

    实现自动播放音乐但是只能放几秒

--- src/view/Music.java
+++ src/view/Music.java
@@ -1,5 +1,6 @@
 package view;
 
+import com.sun.javafx.css.StringStore;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
@@ -20,5 +21,8 @@ public class Music{
         mediaPlayer.setAutoPlay(true);
         mediaPlayer.setVolume(0.8);
         mediaPlayer.setCycleCount(1000000000);
+        mediaPlayer.totalDurationProperty();
+        System.out.println(mediaPlayer.getStatus());
     }
+
 }
