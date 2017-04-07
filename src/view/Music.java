c9452a511030d0391a844920b251231faa24ad0f
wym0120 <598251106@qq.com>
2017-04-07 08:35:27 +0800

    添加的音乐类

--- src/view/Music.java
+++ src/view/Music.java
@@ -1,8 +1,13 @@
 package view;
 
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
 /**
  * Created by DELL on 2017/4/6.
  */
 public class Music {
+    Media media ;
+    MediaPlayer mediaPlayer ;
 
 }
